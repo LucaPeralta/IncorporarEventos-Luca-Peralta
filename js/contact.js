651b9508aba4ccfe2c9059814b92349c1ad45e15
Luca Peralta <paradoxluca44@gmail.com>
2022-08-10 20:07:31 -0300

    nuevos cambios

--- js/contact.js
+++ js/contact.js
@@ -3,8 +3,8 @@ function viewsArrayInput(){
     var inputsValues = document.getElementsByClassName('datoInput'),
     namesValues = [].map.call(inputsValues,function(dataInput) {
         arrayInput.push (dataInput.value);
-   }) ;
+     }) ;
     arrayInput.forEach(function(inputsValuesData){
         console.log("EL DATO ES :" + inputsValuesData);
- });
+    });
 }
